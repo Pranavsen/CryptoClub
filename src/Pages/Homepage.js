cd917f54f1ee6ae150caabebb15e65a74d5771ce
Pranav Sen <pranav.sen2019@vitbhopal.ac.in>
2022-05-25 18:22:52 +0530

    first commit

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from '../componemts/Banner/Banner'
+import CoinsTable from '../componemts/CoinsTable';
+
+const Homepage = () => {
+  return (
+   <div>
+   <Banner />
+   <CoinsTable />
+   </div>
+    
+  );
+};
+
+export default Homepage;
